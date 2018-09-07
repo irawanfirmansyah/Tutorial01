09e8a5ff499bff4520ec2fd3d1d5a9e1a78b6bd7
Irawan Firmansyah <firmansyah_irawan@yahoo.co.id>
2018-09-07 17:12:49 +0700

    Initial Commit Mahasiswa.java

---
+++ Mahasiswa.java
@@ -0,0 +1,36 @@
+public class Mahasiswa {
+
+    String nama;
+    int npm;
+    int umur;
+
+    public Mahasiswa(String nama, int npm, int umur) {
+        this.nama = nama;
+        this.npm = npm;
+        this.umur = umur;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public int getNpm() {
+        return npm;
+    }
+
+    public void setNpm(int npm) {
+        this.npm = npm;
+    }
+
+    public int getUmur() {
+        return umur;
+    }
+
+    public void setUmur(int umur) {
+        this.umur = umur;
+    }
+}
